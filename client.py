f711f6043114f32d3ed8d507feba8fb516578151
Tyler Miller <tmill409@cs.utexas.edu>
2022-02-13 14:09:51 -0600

    added main function

--- client.py
+++ client.py
@@ -1,11 +1,6 @@
 from socket import *
 import random
 
-MAGIC = 0xC356
-VERSION = 1
-SEQ_NUM = 0
-SESSION_ID = random.randint(0,100)
-
 def message_packing(command,message=None):
     m = MAGIC.to_bytes(2,byteorder='big')
     v = VERSION.to_bytes(1,byteorder='big')
@@ -17,17 +12,24 @@ def message_packing(command,message=None):
         return b''.join((header,message.encode('ASCII')))
     return header
 
-serverName = 'aristotle.cs.utexas.edu' # CHECK TO MAKE SURE THIS IS CORRECT BEFORE RUNNING - note to self
-serverPort = 5000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
+if __name__ == '__main__':
 
-message = input('Input message: ')
-packet = message_packing(0, message) # Default is 0 until handshaking is implemented
-print(packet)
+    MAGIC = 0xC356
+    VERSION = 1
+    SEQ_NUM = 0
+    SESSION_ID = random.randint(0,100)
 
-clientSocket.sendto(packet,(serverName, serverPort))
+    serverName = 'aristotle.cs.utexas.edu' # CHECK TO MAKE SURE THIS IS CORRECT BEFORE RUNNING - note to self
+    serverPort = 5000
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode())
+    message = input('Input message: ')
+    packet = message_packing(0, message) # Default is 0 until handshaking is implemented
+    print(packet)
 
-clientSocket.close()
+    clientSocket.sendto(packet,(serverName, serverPort))
+
+    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+    print(modifiedMessage.decode())
+
+    clientSocket.close()
