08a1411e626a1907deab1d8148002ddf197de8e3
Bryce-59 <77942717+Bryce-59@users.noreply.github.com>
2022-02-13 23:22:29 -0600

    implemented DATA and ALIVE. added TODO

--- client.py
+++ client.py
@@ -1,3 +1,6 @@
+#TODO: Implement GOODBYE
+#TODO: Implement Error Handling
+
 from socket import *
 import random
 from message import *
@@ -7,6 +10,12 @@ serverPort = 5000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 session_id = random.randint(0,(2**31) - 1)
 
+"""
+A function that accepts a command, sequence number, and (if command == Command.DATA)
+a string to append.
+The function packs and transmits the message to the server, and waits for a response.
+This response is then analyzed and then, if valid, the command is returned.
+"""
 def send_and_receive(command, seq_num, data=None):
     message = message_packing(command, seq_num, session_id)
     if (command == Command.DATA):
@@ -28,13 +37,13 @@ if __name__ == '__main__':
         print("error") #delete
     "End Handshake"
 
-    while (True): #Implement so that server returns ALIVE after receiving DATA 
+    while (True):
         data = input('Input message: ')
 
         rcv_cmd = send_and_receive(Command.DATA, seq_num, data)
         seq_num += 1
-
-        modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-        print(modifiedMessage.decode())
+        if (rcv_cmd != Command.ALIVE):
+            #maybe an error, hard to tell
+            print("Not ALIVE?")
 
     clientSocket.close()
